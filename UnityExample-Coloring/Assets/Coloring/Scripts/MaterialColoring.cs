ca3ab560d7030a81cf6092ef01ecaa8ba2b2b755
bluefallsky <bluefallsky@naver.com>
2015-08-23 18:43:57 +0900

    gen material coloring scene

---
+++ UnityExample-Coloring/Assets/Coloring/Scripts/MaterialColoring.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialColoring : LoopSampleColor
+{
+	[SerializeField]
+	Renderer renderer;
+
+	Material material;
+
+	void Awake()
+	{
+		material = renderer.material;
+	}
+
+	void Update()
+	{
+		base.Update();
+
+		material.color = Sample();
+	}
+}
---
+++ UnityExample-Coloring/Assets/Coloring/Scripts/SampleColor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopSampleColor : MonoBehaviour
+{
+	#region Gradient
+	[SerializeField]
+	Gradient color;
+	#endregion
+
+	[SerializeField]
+	[Range(0.001f,5.0f)]
+	float duration;
+
+	float curTime=0.0f;
+
+	public Color Sample()
+	{
+		return color.Evaluate( curTime/duration );
+	}
+
+	#region Update Animation time
+	protected void Update()
+	{
+		if( curTime < duration )
+		{
+			curTime+=Time.deltaTime;
+		}
+		else
+		{
+			TrimTime();
+		}
+	}
+
+	void TrimTime()
+	{
+		curTime -= duration;
+	}
+	#endregion
+}
